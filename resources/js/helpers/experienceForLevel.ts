ec125c3eef432eda0083d55bb4f1208f6d3561c4
MJZwart <prorenata.blue@gmail.com>
2023-12-29 14:26:45 +0100

    Lower base exp to have round number

--- resources/js/helpers/experienceForLevel.ts
+++ resources/js/helpers/experienceForLevel.ts
@@ -1,5 +1,5 @@
 export const experienceCapForLevel = (level: number): number => {
-    const baseExp = 1000;
+    const baseExp = 950;
     const cap = baseExp + 50 * ((level * (level + 1) * (2 * level + 1)) / 6);
     return cap;
 };
