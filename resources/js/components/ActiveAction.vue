97db5fb57c98bcb84029176c50eaa8f0f76b3491
MJZwart <prorenata.blue@gmail.com>
2024-02-09 15:22:12 +0100

    Installed vue router and let it do the heavy lifting

--- resources/js/components/ActiveAction.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="p-3">
-        <component :is="activeTab?.component" />
-    </div>
-</template>
-
-<script setup lang="ts">
-import {activeTab} from '../service/tabService';
-</script>
